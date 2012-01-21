4f286d1b93e392ea35ef8cefebf8ec5533a5557b
luke <luke.lkj@taobao.com>
2012-01-22 00:08:31 +0800

    Initial Commit

---
+++ socketClient/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  socketClient
+//
+//  Created by Lu Kejin on 1/22/12.
+//  Copyright (c) 2012 Taobao.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
