00b2f023e7bd61cbacc95734b07e6c9d1e3992c7
luke <luke.lkj@taobao.com>
2012-01-24 23:18:24 +0800

    使用select改造socketClient

--- socketClient/main.c
+++ socketClient/main.c
@@ -11,6 +11,10 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
 
 int main (int argc, const char * argv[])
 {
@@ -21,33 +25,64 @@ int main (int argc, const char * argv[])
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     bzero(&(server_addr.sin_zero),8);
     
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
+    int server_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock_fd == -1) {
         perror("socket error");
         return 1;
     }
-    char recv_msg[1024];
-    char reply_msg[1024];
+    char recv_msg[BUFFER_SIZE];
+    char input_msg[BUFFER_SIZE];
     
-    if (connect(server_socket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in))==0) {
+    if (connect(server_sock_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in))==0) {
+        fd_set client_fd_set;
+        struct timeval tv;
+        tv.tv_sec = 20;
+        tv.tv_usec = 0;
+
+        
         while (1) {
-            bzero(recv_msg, 1024);
-            bzero(reply_msg, 1024);
-            long byte_num = recv(server_socket,recv_msg,1024,0);
-            recv_msg[byte_num] = '\0';
-            printf("server said:%s\n",recv_msg);
+            FD_ZERO(&client_fd_set);
+            FD_SET(STDIN_FILENO, &client_fd_set);
+            FD_SET(server_sock_fd, &client_fd_set);
             
-            printf("reply:");
-            scanf("%s",reply_msg);
-            if (send(server_socket, reply_msg, 1024, 0) == -1) {
-                perror("send error");
+            int ret = select(server_sock_fd + 1, &client_fd_set, NULL, NULL, &tv);
+            if (ret < 0 ) {
+                printf("select 出错!\n");
+                continue;
+            }else if(ret ==0){
+                printf("select 超时!\n");
+                continue;
+            }else{
+                if (FD_ISSET(STDIN_FILENO, &client_fd_set)) {
+                    bzero(input_msg, BUFFER_SIZE);
+                    fgets(input_msg, BUFFER_SIZE, stdin);
+                    if (send(server_sock_fd, input_msg, BUFFER_SIZE, 0) == -1) {
+                        perror("发送消息出错!\n");
+                    }
+                }
+                
+                if (FD_ISSET(server_sock_fd, &client_fd_set)) {
+                    bzero(recv_msg, BUFFER_SIZE);
+                    long byte_num = recv(server_sock_fd,recv_msg,BUFFER_SIZE,0);
+                    if (byte_num > 0) {
+                        if (byte_num > BUFFER_SIZE) {
+                            byte_num = BUFFER_SIZE;
+                        }
+                        recv_msg[byte_num] = '\0';
+                        printf("服务器:%s\n",recv_msg);
+                    }else if(byte_num < 0){
+                        printf("接受消息出错!\n");
+                    }else{
+                        printf("服务器端退出!\n");
+                        exit(0);
+                    }
+
+                }
             }
         }
         
     }
     
-    // insert code here...
-    printf("Hello, World!\n");
     return 0;
 }
 
